3bb28dd00842ad369926f5be7ad19fa2cb3c8454
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-03-20 17:15:25 +0100

    initial container testing environment with controller, connectors and poem

---
+++ docker/docker-run-controller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# -v "${HOME}":/home/user/ \
+
+docker run \
+--cap-add=SYS_ADMIN \
+--device /dev/snd \
+--device /dev/dri \
+-e "DISPLAY=unix$DISPLAY" \
+-e "SSH_AUTH_SOCK=${SSH_AUTH_SOCK}" \
+--mount type=bind,src="${SSH_AUTH_SOCK}",target="${SSH_AUTH_SOCK}" \
+--log-driver json-file \
+--log-opt max-size=10m \
+-v /dev/log:/dev/log \
+-v /etc/localtime:/etc/localtime \
+-v "${HOME}":/mnt/ \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-h controller \
+--net host \
+--name controller \
+--rm -ti \
+auto-controller /bin/zsh
+
+# -v "${HOME}"/.ssh:/home/user/.ssh/ \
